3029ecc1eb5feceba80ba1ce5f39c240e168b8e7
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-02 12:55:39 +1300

    feat: social logins and back button

---
+++ components/auth/card-wrapper.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { Header } from '@/app/auth/header';
+import { Card, CardContent, CardFooter, CardHeader } from '../ui/card';
+import { Social } from './social';
+import { BackButton } from './back-button';
+
+interface CardWrapperProps {
+  children: React.ReactNode;
+  headerLabel: string;
+  backButtonLabel: string;
+  backButtonHref: string;
+  showSocial?: boolean;
+}
+
+export const CardWrapper = ({
+  children,
+  headerLabel,
+  backButtonHref,
+  backButtonLabel,
+  showSocial,
+}: CardWrapperProps) => {
+  return (
+    <Card className="w-[400px] shadow-md">
+      <CardHeader>
+        <Header label={headerLabel} />
+      </CardHeader>
+      <CardContent>{children}</CardContent>
+      {showSocial && (
+        <CardFooter>
+          <Social />
+        </CardFooter>
+      )}
+
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
+    </Card>
+  );
+};
