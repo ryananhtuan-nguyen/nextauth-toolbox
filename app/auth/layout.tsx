8c8a41726581a527dd1f4351b5858a1f6d7c94ec
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-02 12:45:32 +1300

    style: change background color

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import React, { PropsWithChildren } from 'react';
 
 const AuthLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className="h-full flex items-center justify-center bg-pink-200">
+    <div className="h-full flex items-center justify-center bg-pink-200 bg-gradient-to-r from-indigo-500 to-indigo-100">
       {children}
     </div>
   );
