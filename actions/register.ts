9d369b56814d5d9bbd314c235ea716ac768aa52d
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-03 13:21:16 +1300

    feat: change to db function

--- actions/register.ts
+++ actions/register.ts
@@ -1,7 +1,10 @@
 'use server';
-
-import { RegisterSchema } from '@/schemas';
 import { z } from 'zod';
+import bcrypt from 'bcrypt';
+
+import { getUserByEmail } from '@/data/user';
+import { db } from '@/lib/db';
+import { RegisterSchema } from '@/schemas';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,23 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields!' };
   }
 
-  console.log(validateFields);
-  return { success: 'Email sent' };
+  const { email, name, password } = validateFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+  if (existingUser) {
+    return { error: 'Email already in use!' };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  //TODO: Send verification token email
+
+  return { success: 'User Created' };
 };
