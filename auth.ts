8c39b0c008fb0b4deff754ad4a521b8bb6e62440
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-05 14:34:37 +1300

    fix: export Signin/out

--- auth.ts
+++ auth.ts
@@ -3,7 +3,12 @@ import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from './lib/db';
 import authConfig from './auth.config';
 
-export const { handlers, auth } = NextAuth({
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
